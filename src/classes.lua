f4c7adbe673634b5a46a5041fe79bbab83cefd00
Nightmare0 <seller.nightmares@gmail.com>
2017-02-17 02:26:18 +0600

    Update classes.lua

--- src/classes.lua
+++ src/classes.lua
@@ -1,37 +1,44 @@
 --- Скрипт реализующий модель OOP на стыке модулей и метатаблиц.
 --@script
+-- 	У классов есть две модели создания. Первая: Каждый наследуемый объект индексируется классом, конструктор объекта принимает
+-- в аргумент экземпляры наследуемых объектов.
+-- Вторая: Первый аргумент конструктора объекта self представляет конечный объект класса и содержит в себе все публичные методы
+-- наследуемых и текущего классов. (!)
 
 require "functional"
 
---[[
 if functional.is_c_function(type) then
-	local _type = type
+	rawtype = type
 	function type(arg)
 		local mt = getmetatable(arg)
-		if not mt then return _type(arg) end
+		if rawtype(mt) ~= "table" then return rawtype(arg) end
 		if mt.__type ~= nil then
 			assert(type(mt.__type) == "string", "mt.__type must be string value.")
 			return mt.__type
 		end
-		return _type(arg)
+		return rawtype(arg)
 	end
 end
-]]
+
+local combine 	= functional.combine_indexing_with
+local impose 	= functional.impose_of_two
+local reverse 	= functional.reverse
+local insert 	= table.insert
+local setfenv 	= setfenv
+local getfenv 	= getfenv
+local pairs, ipairs = pairs, ipairs
 
 --- Таблица пространств классов.
 --@local
 --@table _classes
-local _classes = {}
+--local
+_classes = _classes or {}
 
---- Проверить vararg На правильность структуры.
---@tparam vararg ...
---@local
-local function _check_string_classes(...)
-	for i, str in ipairs({...}) do
-		assert(type(str) == "string")
-		assert(_classes[str] ~= nil, "Attempt to inherit from non existance class=" .. str ..".")
-	end
-end
+local function assert_name(name) assert(type(name) == "string" and _classes[name] ~= nil, "Not valid Class Name " .. name) end
+local function assert_names(...) for i, str in ipairs({...}) do assert_name(str) end end
+local function get_class(name) return _classes [name] end
+
+do -- Class Declaration Space
 
 --- Новое пространство для класса.
 --@tparam string name Название создаваемого класса
@@ -39,13 +46,20 @@ end
 --@treturn table _t
 local function _new_env(name, ...)
 	local _t = {
-		_M = {}, -- Статическое пространство класса
-		_I = {}, -- Системная информация о классе.
+		{_NAME = name}, -- Статическое пространство класса
+		_C = false, -- Функция конструкции объекта. (оптимизация см. далее)
+		            -- false для построения функции. (!)
 	}
-	for i, str in ipairs({...}) do
-		table.insert(_t._I, _classes[str]._M )
+	if #{...} > 0 then
+		for i, str in ipairs({...}) do
+			table.insert(_t, _classes[str] [1])
+		end
+		local ind = {}
+		for i = 2, #_t do
+			ind[i-1] = _t[i]
+		end
+		combine(_t[1], ind)
 	end
-	functional.combine_indexing_with(_t._M, _t._I)
 	return _t
 end
 
@@ -53,123 +67,197 @@ end
 --@tparam string name Название класса
 --@tparam vararg ... Список наследования.
 function class(name, ...)
-	_check_string_classes(...)
-	local _t = _classes [name]
-	if not _t then
-		_t = _new_env(name, ...)
-		_classes [name] = _t
-		_t._M._NAME = name -- Название класса.
-	end
-	setfenv(2, _t._M)
+	assert_names(...)
+	local _t = _new_env(name, ...)
+	local _M = _t [1]
+	_classes[name] = _t
+	setfenv(2, _M)
+
+	local mods = {}
+	setmetatable(mods, {__index = function()
+		return function()
+			return mods
+		end
+	end})
+	return mods
 end
 
---- Связать объект со сборщиком биомусора. :D
+end --//Class Declaration Space
+
+do --Class Creation Space
+
+local function _get_metas(_M)
+	local mt = {}
+	for k, v in pairs(_M) do
+		if k:sub(1, 2) == "__" then
+			mt[k] = v
+			--_M[k] = v
+		end
+	end
+	return mt
+end
+
+local rawget = rawget
+
+--- Вызвать конструктор объекта, собрать информацию о метаполях.
+-- Вариант 1: Индексация наследуемых объектов через .__index
+-- Недостаток: Каждый объект отвечает сам за себя.
+--@tparam table _M Статическое окружения
+--@tparam table objects Экземпляры наследников.
+--@tparam table mt Собираемая метатаблица
+--@tparam table obj финальный объект
+local function _do_1(_M, objects, mt, dtors)
+	local _mt = _get_metas(_M)
+	impose(mt, _mt)
+	local _C = rawget(_M, "New")
+	if not _C then
+		print(_M._NAME, "have no object constructor.")
+		return
+	end
+
+	local self = setmetatable({}, _mt)
+	combine(self, objects) ---{_M, unpack(objects)}) -- access to its parent objects
+	setfenv(_C, self)
+	_C(self, unpack(objects))
+	setfenv(_C, {})
+	insert(dtors, self.dtor)
+	print(_M._NAME, "dtor pushed")
+	return self
+end
+
+--[[
+local function _perform_ctor(obj, self)
+	print(obj, self)
+	local _ctor = rawget(self, "ctor")
+	if not _ctor then
+		return
+	end
+	rawset(self, "ctor", nil)
+	local ctor = rawget(obj, "ctor")
+	if not ctor then
+		rawset(obj, "ctor", _ctor)
+	else
+		rawset(obj, "ctor", function(...)
+			_ctor(...) ctor(...)
+		end )
+	end
+end
+]]
+
+local _inheritance_block = {}
+
+--- Вызвать конструктор объекта, собрать информацию о метаполях.
+-- Вариант 2: Объект все свои публичные методы сохраняет во внешней таблице.
+local function _do_2(_M, objects, mt, dtors, obj)
+	impose(mt, _get_metas (_M))
+	impose(getmetatable(obj), mt)
+	local _C = rawget(_M, "New")
+	if not _C then
+		print(_M._NAME, "have no object constructor.")
+		return
+	end
+	mt.__type = _M._NAME
+	local self = setmetatable({}, {})
+	impose(getmetatable(self), mt)
+	combine(self, {_M, obj})
+	setfenv (_C, self)
+	_C(self, unpack(objects), obj)
+	setfenv (_C, {})
+	insert(dtors, rawget(self, "dtor"))
+	impose(obj, self)
+	return self
+end
+
+--- Построить объект.
+--@tparam table _t Объект
+local function _construct(_t, mt, dtors, obj)
+	local objects = {}
+	local l = #_t
+	local mt = mt or {}
+	if l > 1 then
+		for i = 2, l do -- Собираем наследников рекурсивно
+			local _M = _t [i]
+			impose(mt, _get_metas(_M))
+			local obj = _construct(_classes[_M._NAME], mt, dtors, obj)
+			insert(objects, obj or nil)
+		end
+	end
+	local _M = _t [1]
+	return _do_2(_M, objects, mt, dtors, obj) -- Для текущего класса. (!)
+end
+
+--- Активировать объект (установить сборщик мусора)
 --@tparam table obj
-local function _set_gc(mt, obj)
-	local objmt = mt
+--@tparam table mt
+local function _activate(obj, mt)
 	if _VERSION == "Lua 5.1" then
-		local prox = newproxy (true)
+		local prox = newproxy(true)
 		getmetatable(prox).__gc = obj.dtor
 		mt.prox = prox
 	else
-		getmetatable(obj).__gc = obj.dtor
+		mt.__gc = obj.dtor
 	end
 end
 
---- Произвести шаг сборки объекта.
---@tparam function _C функция сборки класса.
--- Определяется в классе функцией с названием _
---@tparam table _M статическая таблица класса.
---@tparam table obj Ссылка на собираемый объект.
---@tparam table dtors Ссылка на таблицу для заполнения деструкторами.
-local function _do_build(_C, _M, obj, dtors)
-	local _pub = {} -- Таблица сборки.
-	functional.combine_indexing_with(_pub, {_M, obj})
-	setfenv(_C, _pub)
-	_C( obj ) -- Вызов сборщика объекта.
-	local acc_ctor = rawget(_pub, "ctor")
-	if acc_ctor then
-		if not obj.ctor then
-			obj.ctor = acc_ctor
-		else
-			local ctor = obj.ctor
-			obj.ctor = function(...)
-				ctor (...)
-				acc_ctor (...)
-			end
-		end
-		rawset(_pub, "ctor", nil)
-	end
-	for k, v in pairs(_pub) do
-		obj [k] = v -- Объединяем публичные ключи с создаваемым объектом.
-	end
-	table.insert(dtors, rawget(_pub, "dtor"))
-end
-
---- Деактивировать объекта obj.
+--- Деактивировать (уничтожить) объект.
 --@tparam table obj
 local function _deactivate(obj)
 	local mt = getmetatable(obj)
 	if mt.prox then
 		getmetatable(mt.prox).__gc = nil
-	else
-		mt.__gc = nil
+	end
+	for k in pairs(obj) do
+		obj [k] = nil
+		--rawset(obj, k, nil)
 	end
 	setmetatable(obj, nil)
 end
 
---- Собрать деконструктор объекту.
---@tparam table obj
---@tparam table dtors
-local function _set_dtors (obj, dtors)
-	if #dtors < 2 then
-		obj.dtor = dtors[1]
-	else
-		obj.dtor = function (...)
-			for i = #dtors, 1, -1 do
-				dtors [i] (...)
+local rawset = rawset
+
+--- Установить деконструктор объекту.
+-- @tparam table obj
+-- @tparam table dtors
+local function _dtors(obj, dtors)
+	reverse(dtors)
+	rawset(obj, "dtor", function()
+		for i = 1, #dtors do
+			--dtors[i] ()
+			local b, msg = pcall(dtors[i])
+			if not b then
+				print("Error in .dtor: " .. msg)
 			end
-			obj.dtor = nil
-			_deactivate (obj)
 		end
-	end
+		_deactivate (obj)
+	end	)
 end
 
-local function _merge_metamethods (_M, mt)
-	for k, v in pairs(_M) do
-		if k:match("^__") then
-			mt[k] = v
-			_M[k] = nil
-		end
+local pcall = pcall
+
+--- Функция конструкции объекта.
+--@tparam string name Название класса.
+--@tparam vararg ... Аргументы конструктора.
+function _Class (name, ...)
+	assert_name(name)
+	local _t = _classes [name]
+	local dtors = {}
+	local pool = setmetatable({}, {}) -- Object pool of public keys (fields, methods)
+	local self = _construct(_t, nil, dtors, pool)
+	--_inheritance_block = {}
+	_dtors(self, dtors)
+	_activate(self, getmetatable(self))
+
+	combine(pool, {_t [1]})
+	if self.ctor ~= nil then
+		self.ctor(...)
+		self.ctor = nil
 	end
+	return self
 end
 
---- Собрать объект.
---@tparam table _t _M/_I pair
-local function _run_construction(_t)
-	local dtors, blocks, mt =
-		{}, {}, {__type = _t._M._NAME}
-	local function construct(_t, obj)
-		local _M = _t._M
-		--assert(not blocks[_M._NAME], "Cycle inheritance found.")
-		--blocks[_M._NAME] = true
-		for i = #_t._I, 1, -1 do
-			local _M = _t._I [i]
-			construct (_classes[_M._NAME] , obj)
-		end
-		_merge_metamethods (_M, mt)
-		local _C = _M._
-		if not _C then return end
-		_do_build(_C, _M, obj, dtors)
-	end
-	local obj = {}
-	construct (_t, obj)
-	_set_dtors(obj, dtors)
-	_set_gc(mt, obj)
-	setmetatable(obj, mt)
-	functional.combine_indexing_with(obj, {_t._M})
-	return obj
-end
+end --// Class Creation Space
+
+
 do
 	local mt = {}
 	mt.__index = mt
@@ -178,26 +266,7 @@ do
 	--@tparam string name
 	--@tparam vararg ... Наследование.
 	function mt:__call(name, ...)
-		local _t = _classes [name]
-		assert(_t ~= nil, "Cannot create non existance class.")
-		local obj = _run_construction (_t)
-		obj.ctor(...)
-		obj.ctor = nil
-		return obj
-	end
-
---	function mt:__index(name)
---		return function(...)
---			return self(name, ...)
---		end
---	end
-
-	function mt.GetClasses()
-		local t = {}
-		for k, v in pairs(_classes) do
-			table.insert(t, v._M)
-		end
-		return t
+		return _Class (name, ...)
 	end
 
 --[[GLOBAL]] Class = setmetatable({}, mt)
